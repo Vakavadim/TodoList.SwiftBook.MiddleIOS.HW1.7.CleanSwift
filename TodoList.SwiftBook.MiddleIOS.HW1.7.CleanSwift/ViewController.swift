9ad8d820ca3a7370ab225ef08834c30588946c62
Vadim Gamzaev <Vadosbox@gmail.com>
2023-02-22 15:10:28 +0300

    Initial Commit

---
+++ TodoList.SwiftBook.MiddleIOS.HW1.7.CleanSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoList.SwiftBook.MiddleIOS.HW1.7.CleanSwift
+//
+//  Created by Вадим Гамзаев on 22.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
